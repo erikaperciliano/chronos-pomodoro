646a1488dd8b340b7f612530b36dcb809810175e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-09 15:00:13 -0300

    feat: creates the home page and the template for all other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,8 @@
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { Footer } from './components/Footer';
-
-import { PlayCircleIcon } from 'lucide-react';
-
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='meuInput'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Body Type sheets.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,30 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Body Type sheets.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
