6066ab8f100a6b110b263a0179e75b9724659c0d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-09 16:03:38 -0300

    feat: creates GenericHtml

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
-  return <Home />;
+  return <AboutPomodoro />;
 }
