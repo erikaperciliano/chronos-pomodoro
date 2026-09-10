72f9dd2c2e78f899a1db46ddb3e02730bf919f31
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-09 21:37:14 -0300

    feat: working with state before and after setState(getNextCycle)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,14 +3,51 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContex } from '../contexts/TaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
 
 export function MainForm() {
+  const { state, setState } = useTaskContex();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
 
   function handleCreateNewTask(event: React.ChangeEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log('DEU CERTO!', taskNameInput.current?.value);
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current?.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
