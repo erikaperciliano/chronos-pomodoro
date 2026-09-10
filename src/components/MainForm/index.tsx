69a47be45d4a0e7e48eeb32f1018ce5a0800d7e1
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-09 21:13:02 -0300

    feat: add useRef to prevent component re-rendering

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,15 +2,25 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('DEU CERTO!', taskNameInput.current?.value);
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           placeholder='Digite algo'
+          ref={taskNameInput}
         />
       </div>
 
