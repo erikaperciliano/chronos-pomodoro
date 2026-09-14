f30a9929a7ec78805583662029ab9c6fc9bb7a84
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-14 15:25:54 -0300

    feat: update the controller every second in the page state

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -38,6 +38,22 @@ export function taskReducer(
 
       return state;
     }
+    case TaskActionTypes.COMPLETE_TASK: {
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: '00:00',
+        tasks: state.tasks.map(task => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, completeDate: Date.now() };
+          }
+          return task;
+        }),
+      };
+
+      return state;
+    }
     case TaskActionTypes.RESET_STATE: {
       return state;
     }
